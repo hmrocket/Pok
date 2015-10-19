e00e88d694557ca5613a49c43351db6da308e863
Mhamed <hlioui@thoughttechnology.com>
2015-10-19 13:22:31 -0400

    Code formating use tab

--- poker/src/main/java/com/hmrocket/poker/Game.java
+++ poker/src/main/java/com/hmrocket/poker/Game.java
@@ -13,105 +13,105 @@ import java.util.Set;
  */
 public class Game implements PokerRound.RoundEvent {
 
-    private PokerRound pokerRound;
-    private Pot pot;
-    private CommunityCards communityCards;
-    private Deck deck;
-    private GameEvent gameEventListener;
+	private PokerRound pokerRound;
+	private Pot pot;
+	private CommunityCards communityCards;
+	private Deck deck;
+	private GameEvent gameEventListener;
 
 
-    protected Game() {
-        pot = new Pot();
-        deck = new Deck();
-    }
+	protected Game() {
+		pot = new Pot();
+		deck = new Deck();
+	}
 
 
-    public Game(GameEvent gameEvent) {
-        pot = new Pot();
-        deck = new Deck();
-        gameEventListener = gameEvent;
-    }
+	public Game(GameEvent gameEvent) {
+		pot = new Pot();
+		deck = new Deck();
+		gameEventListener = gameEvent;
+	}
 
-    //TODO POker round should aks for mean bet
-    public void startNewHand(long minBet, List<Player> players, int dealerIndex) {
-        deck.reset();
-        pot.setup(players);
-        communityCards = new CommunityCards();
-        // give players new Hand
-        for (Player player : players) {
-            player.setState(Player.PlayerState.ACTIVE);
-            HandHoldem handHoldem = new HandHoldem(deck.drawCard(), deck.drawCard(), communityCards);
-            player.setHand(handHoldem);
-        }
-        pokerRound = new PokerRound(minBet, players, dealerIndex);
-        pokerRound.startGame();
-    }
+	//TODO POker round should aks for mean bet
+	public void startNewHand(long minBet, List<Player> players, int dealerIndex) {
+		deck.reset();
+		pot.setup(players);
+		communityCards = new CommunityCards();
+		// give players new Hand
+		for (Player player : players) {
+			player.setState(Player.PlayerState.ACTIVE);
+			HandHoldem handHoldem = new HandHoldem(deck.drawCard(), deck.drawCard(), communityCards);
+			player.setHand(handHoldem);
+		}
+		pokerRound = new PokerRound(minBet, players, dealerIndex);
+		pokerRound.startGame();
+	}
 
-    public void removePlayer(Player player) {
-        pokerRound.removePlayer(player);
-    }
+	public void removePlayer(Player player) {
+		pokerRound.removePlayer(player);
+	}
 
-    /**
-     * Poker Round only ends when (1) only one player is left or
-     * (2) all remaining players have matched the highest total bet made during the round.
-     */
-    @Override // TODO Game should check win
-    public void onRoundFinish(RoundPhase phase, List<Player> players) {
-        pot.update();
+	/**
+	 * Poker Round only ends when (1) only one player is left or
+	 * (2) all remaining players have matched the highest total bet made during the round.
+	 */
+	@Override // TODO Game should check win
+	public void onRoundFinish(RoundPhase phase, List<Player> players) {
+		pot.update();
 
-        if (isAllPlayersExceptOneFolded(players)) endGame();
-        else
-            switch (phase) {
-                case PRE_FLOP:
-                    communityCards.setFlop(deck.dealFlop());
-                    break;
-                case FLOP:
-                    communityCards.setTurn(deck.drawCard());
-                    break;
-                case TURN:
-                    communityCards.setRiver(deck.drawCard());
-                    break;
-                case RIVER: // Game ended // the game not always ends here !
-                    showdown();
-                    break;
-            }
-    }
+		if (isAllPlayersExceptOneFolded(players)) endGame();
+		else
+			switch (phase) {
+				case PRE_FLOP:
+					communityCards.setFlop(deck.dealFlop());
+					break;
+				case FLOP:
+					communityCards.setTurn(deck.drawCard());
+					break;
+				case TURN:
+					communityCards.setRiver(deck.drawCard());
+					break;
+				case RIVER: // Game ended // the game not always ends here !
+					showdown();
+					break;
+			}
+	}
 
-    private boolean isAllPlayersExceptOneFolded(List<Player> players) {
-        int numberOfPlayerNotOut = 0;
-        for (Player player :
-                players) {
-            if (player.isOut() == false) numberOfPlayerNotOut++;
-            if (numberOfPlayerNotOut > 1) return false;
-        }
-        return true;
-    }
+	private boolean isAllPlayersExceptOneFolded(List<Player> players) {
+		int numberOfPlayerNotOut = 0;
+		for (Player player :
+				players) {
+			if (player.isOut() == false) numberOfPlayerNotOut++;
+			if (numberOfPlayerNotOut > 1) return false;
+		}
+		return true;
+	}
 
-    /**
-     * when
-     */
-    private void endGame() {
-        Set<Player> busted = pot.distributeToWinners();
-        if (gameEventListener != null) {
-            gameEventListener.playerBusted(busted);
-            gameEventListener.gameEnded();
-        }
-    }
+	/**
+	 * when
+	 */
+	private void endGame() {
+		Set<Player> busted = pot.distributeToWinners();
+		if (gameEventListener != null) {
+			gameEventListener.playerBusted(busted);
+			gameEventListener.gameEnded();
+		}
+	}
 
-    private void showdown() {
-        endGame();
-    }
+	private void showdown() {
+		endGame();
+	}
 
-    @Override
-    public void onRaise() {
+	@Override
+	public void onRaise() {
 
-    }
+	}
 
-    protected interface GameEvent { // Table is expecting a callback when game ends and when a player doesn't have enough money to bet
-        public void gameEnded();
+	protected interface GameEvent { // Table is expecting a callback when game ends and when a player doesn't have enough money to bet
+		public void gameEnded();
 
-        public void playerBusted(Set<Player> player);
-    }
+		public void playerBusted(Set<Player> player);
+	}
 
 
 }
--- poker/src/main/java/com/hmrocket/poker/pot/Pot.java
+++ poker/src/main/java/com/hmrocket/poker/pot/Pot.java
@@ -14,16 +14,16 @@ import java.util.Stack;
  */
 public class Pot {
 
-    private MainPot mainPot;
-    private Stack<SidePot> sidePots;
+	private MainPot mainPot;
+	private Stack<SidePot> sidePots;
 	private long sidePotsTotalValue;
 
 
-    public Pot() {
+	public Pot() {
 		mainPot = new MainPot();
-        sidePots = new Stack<SidePot>();
+		sidePots = new Stack<SidePot>();
 		sidePotsTotalValue = 0;
-    }
+	}
 
 	public Pot(Set<Player> playersInTheGame) {
 		mainPot = new MainPot();
@@ -34,75 +34,74 @@ public class Pot {
 	/**
 	 * empty the Pot
 	 */
-    public void reset() {
-        mainPot.reset();
-        sidePots.clear();
+	public void reset() {
+		mainPot.reset();
+		sidePots.clear();
 		sidePotsTotalValue = 0;
 	}
 
 	public void setup(Set<Player> playersInTheGame) {
-        reset();
-        // we will add all players only once
-        mainPot.init(playersInTheGame);
-    }
+		reset();
+		// we will add all players only once
+		mainPot.init(playersInTheGame);
+	}
 
 	/**
-	 *
 	 * @return Pot total worth
 	 */
 	public long getValue() {
 		return sidePotsTotalValue + mainPot.value;
 	}
 
-    public void update() {
-        if (mainPot.potentialWinners.isEmpty()) // XXX no worries mainPot.potentialWinners will be never null
-            return;
+	public void update() {
+		if (mainPot.potentialWinners.isEmpty()) // XXX no worries mainPot.potentialWinners will be never null
+			return;
 
-        //////// Set Up the sidePots first, If there is any new ones
+		//////// Set Up the sidePots first, If there is any new ones
 
-        Set<Player> allInPlayers = PokerTools.findAllInPlayers(mainPot.potentialWinners);
-        Player allInPlayer;
+		Set<Player> allInPlayers = PokerTools.findAllInPlayers(mainPot.potentialWinners);
+		Player allInPlayer;
 
-        while (allInPlayers.isEmpty() == false) {
-            allInPlayer = Collections.min(allInPlayers, Player.BET_COMPARATOR);
-            allInPlayers.remove(allInPlayer);
+		while (allInPlayers.isEmpty() == false) {
+			allInPlayer = Collections.min(allInPlayers, Player.BET_COMPARATOR);
+			allInPlayers.remove(allInPlayer);
 			mainPot.potentialWinners.remove(allInPlayer); // even if you didn't removed it it's okay (he's bet is already 0)
 
-            // SidePot is created from min allInPlayer and MainPot cause the value of of MainPot will be transferred to the SidePot
+			// SidePot is created from min allInPlayer and MainPot cause the value of of MainPot will be transferred to the SidePot
 			SidePot sidePot = new SidePot(allInPlayer, mainPot);
 
-            for (Player player : mainPot.potentialWinners) {
-                sidePot.addBet(player);
-            }
+			for (Player player : mainPot.potentialWinners) {
+				sidePot.addBet(player);
+			}
 
 			sidePots.add(sidePot);
 			sidePotsTotalValue += sidePot.getValue();
-        }
+		}
 
-        // add pot to the main pot
-        Iterator<Player> iterator = mainPot.potentialWinners.iterator();
-        Player player;
-        while (iterator.hasNext()) {
-            player = iterator.next();
+		// add pot to the main pot
+		Iterator<Player> iterator = mainPot.potentialWinners.iterator();
+		Player player;
+		while (iterator.hasNext()) {
+			player = iterator.next();
 			mainPot.value += player.getBet();
-            player.setBet(0);
-            if (player.isPlaying() == false)
-                iterator.remove();
-        }
-    }
+			player.setBet(0);
+			if (player.isPlaying() == false)
+				iterator.remove();
+		}
+	}
 
 
-    /**
-     * Distribute level pot To Winners
-     *
-     * @return busted players if there's any
-     */
-    public Set<Player> distributeToWinners() {
-        Set<Player> busted = new HashSet<Player>();
-        Set<Player> levelWinners;
+	/**
+	 * Distribute level pot To Winners
+	 *
+	 * @return busted players if there's any
+	 */
+	public Set<Player> distributeToWinners() {
+		Set<Player> busted = new HashSet<Player>();
+		Set<Player> levelWinners;
 
-        // Get Winners of MainPot
-        levelWinners = PokerTools.getWinners(mainPot.potentialWinners);
+		// Get Winners of MainPot
+		levelWinners = PokerTools.getWinners(mainPot.potentialWinners);
 		distribute(levelWinners, mainPot.value);
 
 		//As long there is SidePot, (money) on the table, Add cash to winners
@@ -115,18 +114,19 @@ public class Pot {
 
 			// every SidePot - check if there is busted players
 			// (players went all among and lost)
-			for(Player player :sidePot.getAllInPlayers()) {
+			for (Player player : sidePot.getAllInPlayers()) {
 				if (levelWinners.contains(player))
 					busted.add(player);
 			}
 		}
 
 		return busted;
-    }
+	}
 
 	/**
 	 * Distribute the winning value equally on winners
-	 * @param winners Set of player who has won the Pot
+	 *
+	 * @param winners  Set of player who has won the Pot
 	 * @param potValue Value of winning
 	 */
 	private void distribute(Set<Player> winners, long potValue) {
