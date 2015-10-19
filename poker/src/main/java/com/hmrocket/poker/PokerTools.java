113551250fea778d9a4a48f1d2705e2b5570539e
hmrocket <hmrocket@gmail.com>
2015-10-18 21:19:52 -0400

    Add getWinners() among a list of Players (Potential players i.e Players Aren't out)

--- poker/src/main/java/com/hmrocket/poker/PokerTools.java
+++ poker/src/main/java/com/hmrocket/poker/PokerTools.java
@@ -1,5 +1,11 @@
 package com.hmrocket.poker;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by hmrocket on 15/10/2015.
  */
@@ -14,5 +20,45 @@ public class PokerTools {
 
     public static long getMaxBuyIn(long minBet) {
         return minBet * MAX_BUY_IN_MULTIPLYER;
+    }
+
+    /**
+     * @param potentialWinners players who has chance of winning, use {@link Player#isOut()}
+     * @return one or more winning hand players, null if the potentialWinners is empty or null
+     */
+    public static List<Player> getWinners(List<Player> potentialWinners) {
+        if (potentialWinners == null || potentialWinners.isEmpty())
+            return null;
+
+        List<Player> winners = new ArrayList<Player>();
+        Player winner = Collections.max(potentialWinners); // find best hand
+        do {
+            winners.add(winner);
+            potentialWinners.remove(winner);
+            winner = Collections.max(potentialWinners); // find the next best hand
+            // if next best hand is equal to best hand, add it to winners
+        } while (winner.compareTo(winners.get(0)) == 0);
+
+        return winners;
+    }
+
+    /**
+     * @param potentialWinners players who has chance of winning, use {@link Player#isOut()}
+     * @return one or more winning hand players, null if the potentialWinners is empty or null
+     */
+    public static Set<Player> getWinners(Set<Player> potentialWinners) {
+        if (potentialWinners == null || potentialWinners.isEmpty())
+            return null;
+
+        Set<Player> winners = new HashSet<Player>();
+        Player winner = Collections.max(potentialWinners); // find best hand
+        do {
+            winners.add(winner);
+            potentialWinners.remove(winner);
+            winner = Collections.max(potentialWinners); // find the next best hand
+            // if next best hand is equal to best hand, add it to winners
+        } while (winner.compareTo(winners.iterator().next()) == 0);
+
+        return winners;
     }
 }
