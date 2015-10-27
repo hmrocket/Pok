e9a86a1f4d89faaad0005c4ae396cd22ef8a8ebf
hmrocket <hmrocket@gmail.com>
2015-10-27 02:40:20 -0400

    Add 3 basic bots

---
+++ poker/src/main/java/com/hmrocket/poker/ai/bot/RandBot.java
@@ -0,0 +1,18 @@
+package com.hmrocket.poker.ai.bot;
+
+import com.hmrocket.poker.Player;
+
+/**
+ * Created by hmrocket on 26/10/2015.
+ */
+public class RandBot extends Player {
+
+	public RandBot(String name, long bankBalance, long cash) {
+		super(name, bankBalance, cash);
+	}
+
+	@Override
+	public long play(long amountToContinue) {
+		return super.play(amountToContinue);
+	}
+}
