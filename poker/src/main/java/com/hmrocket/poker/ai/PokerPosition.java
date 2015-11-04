f63986765518f94ef39a411a294d54a74b4624a6
hmrocket <hmrocket@gmail.com>
2015-11-04 01:06:39 -0500

    Include Blinds position and method to calculate position on pre flop phase

--- poker/src/main/java/com/hmrocket/poker/ai/PokerPosition.java
+++ poker/src/main/java/com/hmrocket/poker/ai/PokerPosition.java
@@ -6,7 +6,30 @@ package com.hmrocket.poker.ai;
 public enum PokerPosition {
 	EARLY,
 	MID_POSITION,
-	LATE;
+	LATE,
+	BLINDS;
+
+	/**
+	 * On pre-flop phase, Determine the PokerPosition of a player basing on his position in the game and players count
+	 *
+	 * @param playersCount
+	 * @param position
+	 * @return PokerPosition
+	 */
+	public static PokerPosition getPositionOnPreFlop(int playersCount, int position) {
+		if (playersCount == position)
+			throw new IllegalArgumentException("positions starts from 0 to " + --playersCount);
+		if (playersCount < 3) {
+			return position == 0 ? MID_POSITION : BLINDS;
+		} else {
+			if (position + 2 >= playersCount)
+				// if the player is in the last two position he belongs to the Blinds position
+				return BLINDS;
+				// it's like removing the 2 last Blinds players
+				// and applying the other rules other
+			else return getPosition(playersCount - 2, position);
+		}
+	}
 
 	/**
 	 * Determine the PokerPosition of a player basing on his position in the game and players count
