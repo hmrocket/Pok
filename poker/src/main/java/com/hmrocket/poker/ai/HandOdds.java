7b0d95dd325eeb92272d7c2698c96b6595648432
hmrocket <hmrocket@gmail.com>
2015-11-06 21:56:13 -0500

    Optimize HandOdds to hold winning percentage related to HandType only

--- poker/src/main/java/com/hmrocket/poker/ai/HandOdds.java
+++ poker/src/main/java/com/hmrocket/poker/ai/HandOdds.java
@@ -1,59 +1,56 @@
 package com.hmrocket.poker.ai;
 
 import com.hmrocket.poker.card.HandType;
-import com.hmrocket.poker.card.Rank;
 
 /**
+ * Holder of winning percentages
  * Created by hmrocket on 26/10/2015.
  */
 public class HandOdds {
-	private float[][] odds;
+	/**
+	 * represent the ods of a specific HandType
+	 */
+	private float[] odds;
+	/**
+	 * Game simulation count
+	 */
+	private int n;
 
-	public HandOdds() {
-		odds = new float[HandType.STRAIGHT_FLUSH.ordinal() + 1][Rank.ACE.ordinal() + 1];
+	/**
+	 * @param n Game simulation count
+	 */
+	public HandOdds(int n) {
+		odds = new float[HandType.ROYAL_FLUSH.ordinal() + 1];
+		this.n = n;
 	}
 
-	public void setOdds(HandType handType, Rank rank, float percentage) {
-		if (handType == HandType.ROYAL_FLUSH) {
-			handType = HandType.STRAIGHT_FLUSH;
-			rank = Rank.ACE;
-		}
-		odds[handType.ordinal()][rank.ordinal()] = percentage;
+	/**
+	 * @param handType   winning HandType
+	 * @param winsCounts numbers of time you win the game with
+	 */
+	public void setOdds(HandType handType, int winsCounts) {
+		odds[handType.ordinal()] = winsCounts;
 	}
 
-	public float getOdds(HandType handType, Rank rank) {
-		if (handType == HandType.ROYAL_FLUSH) {
-			handType = HandType.STRAIGHT_FLUSH;
-			rank = Rank.ACE;
-		}
 
-		return odds[handType.ordinal()][rank.ordinal()];
-	}
-
+	/**
+	 * occurrence of winning with specific HandType / simulation count (n)
+	 * @param handType
+	 * @return percentage of winning with a specific HandType
+	 */
 	public float getOdds(HandType handType) {
-		if (handType == HandType.ROYAL_FLUSH) {
-			handType = HandType.STRAIGHT_FLUSH;
-		}
-		float strength = 0;
-		float[] table = odds[handType.ordinal()];
-		int rankCount = table.length;
-		for (int rank = 0; rank < rankCount; rank++) {
-			float odd = table[rank];
-			strength += odd * (handType.ordinal() * rankCount + rank + 1);
-		}
-		return strength;
+		return odds[handType.ordinal()] / n;
 	}
 
+	/**
+	 * occurrence of winning with all HandType / simulation count (n)
+	 * @return the percentage of winning a game
+	 */
 	public float getHandStrength() {
 		float strength = 0;
-		for (int handType = 0; handType < odds.length; handType++) {
-			float[] table = odds[handType];
-			int length = table.length;
-			for (int rank = 0; rank < length; rank++) {
-				float odd = table[rank];
-				strength += odd * (handType * length + rank + 1);
-			}
+		for (float oddsCount : odds) {
+			strength += oddsCount;
 		}
-		return strength;
+		return strength / n;
 	}
 }
