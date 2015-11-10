2a9a5258f992b302c1167c952a9f5c3b8022e022
Mhamed <hlioui@thoughttechnology.com>
2015-11-10 16:56:21 -0500

    create view classes (PlayerView, PotView, CardView, ChipsView)

---
+++ app/src/main/java/com/hmrocket/magicpoker/view/CardView.java
@@ -0,0 +1,7 @@
+package com.hmrocket.magicpoker.view;
+
+/**
+ * @since 10/Nov/2015 - mhamed
+ */
+public class CardView {
+}
---
+++ app/src/main/java/com/hmrocket/magicpoker/view/PotView.java
@@ -0,0 +1,8 @@
+package com.hmrocket.magicpoker.view;
+
+/**
+ * @since 10/Nov/2015 - mhamed
+ */
+public class PotView {
+
+}
