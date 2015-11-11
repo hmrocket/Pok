de5aa22ebecd2ff62cc8720c6fe9907cd4add747
Mhamed <hlioui@thoughttechnology.com>
2015-11-11 17:28:57 -0500

    implement PotView

---
+++ app/src/main/java/com/hmrocket/magicpoker/Util.java
@@ -0,0 +1,12 @@
+package com.hmrocket.magicpoker;
+
+/**
+ * @since 11/Nov/2015 - mhamed
+ */
+public final class Util {
+
+	public static String formatNumberShort(long amount) {
+		// TODO implement long to string pot
+		return "100K";
+	}
+}
--- app/src/main/java/com/hmrocket/magicpoker/view/PotView.java
+++ app/src/main/java/com/hmrocket/magicpoker/view/PotView.java
@@ -1,8 +1,40 @@
 package com.hmrocket.magicpoker.view;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.hmrocket.magicpoker.R;
+import com.hmrocket.magicpoker.Util;
+
+//TODO multi-size support for the bck, textSize, size en general
+//optional currency support
 /**
  * @since 10/Nov/2015 - mhamed
  */
-public class PotView {
+public class PotView extends FrameLayout {
+
+	private long amount;
+	private TextView potAmountLabel;
+	private ChipView potChips;
+
+	public PotView(Context context, AttributeSet attributeset) {
+		super(context, attributeset);
+		LayoutInflater.from(context).inflate(R.layout.pot_view, this, true);
+		potAmountLabel = (TextView) findViewById(R.id.tx_PotAmount);
+		potChips = (ChipView) findViewById(R.id.chipsView_Pot);
+	}
+
+	public long getAmount() {
+		return amount;
+	}
+
+	public void setAmount(long l) {
+		amount = l;
+		potAmountLabel.setText(Util.formatNumberShort(l));
+		potChips.setChipLevel(l);
+	}
 
 }
