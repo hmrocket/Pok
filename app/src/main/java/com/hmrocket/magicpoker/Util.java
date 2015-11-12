5a9568e9689014111f9fe4caaacb3afc486cd3f5
Mhamed <hlioui@thoughttechnology.com>
2015-11-12 11:26:20 -0500

    implement CardView custom View class

--- app/src/main/java/com/hmrocket/magicpoker/Util.java
+++ app/src/main/java/com/hmrocket/magicpoker/Util.java
@@ -1,5 +1,7 @@
 package com.hmrocket.magicpoker;
 
+import com.hmrocket.poker.card.Card;
+
 /**
  * @since 11/Nov/2015 - mhamed
  */
@@ -8,5 +10,10 @@ public final class Util {
 	public static String formatNumberShort(long amount) {
 		// TODO implement long to string pot
 		return "100K";
+	}
+
+	public static int getCardImageId(Card card) {
+		//TODO implement
+		return R.mipmap.ic_launcher;
 	}
 }
--- app/src/main/java/com/hmrocket/magicpoker/view/CardView.java
+++ app/src/main/java/com/hmrocket/magicpoker/view/CardView.java
@@ -1,7 +1,41 @@
 package com.hmrocket.magicpoker.view;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import com.hmrocket.magicpoker.Util;
+import com.hmrocket.poker.card.Card;
+
 /**
  * @since 10/Nov/2015 - mhamed
  */
-public class CardView {
+public class CardView extends ImageView {
+
+	private Card card;
+
+	public CardView(Context context) {
+		super(context);
+	}
+
+	public CardView(Context context, AttributeSet attributeset) {
+		super(context, attributeset);
+	}
+
+	public Card getCard() {
+		return card;
+	}
+
+	public void setCard(Card card) {
+		this.card = card;
+		if (this.card != null) {
+			// setImageResource ==> reading and decoding on the UI thread
+			// setImageDrawable ==> just the setting is on UI thread
+			setImageResource(Util.getCardImageId(card));
+		} else {
+			setImageBitmap(null);
+		}
+	}
+
 }
+
