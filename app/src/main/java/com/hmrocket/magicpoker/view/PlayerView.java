69fefe204cb8c553b2c8421bc253943c9d7eb0d8
Mhamed <hlioui@thoughttechnology.com>
2015-11-13 15:06:45 -0500

    setup PlayerView layout (profile image, cards, cash)

--- app/src/main/java/com/hmrocket/magicpoker/view/PlayerView.java
+++ app/src/main/java/com/hmrocket/magicpoker/view/PlayerView.java
@@ -1,7 +1,89 @@
 package com.hmrocket.magicpoker.view;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.hmrocket.magicpoker.R;
+import com.hmrocket.magicpoker.Util;
+import com.hmrocket.poker.Player;
+import com.hmrocket.poker.card.Hand;
+
 /**
  * @since 10/Nov/2015 - mhamed
  */
-public class PlayerView {
+public class PlayerView extends RelativeLayout {
+
+	// XXX Assure the View save it's state
+	private CardView cardView1;
+	private CardView cardView2;
+	private ImageView profileImage;
+	private TextView txCash;
+
+	public PlayerView(Context context) {
+		super(context);
+		LayoutInflater.from(context).inflate(R.layout.player_view, this, true);
+		profileImage = (ImageView) findViewById(R.id.iv_profile);
+		cardView1 = (CardView) findViewById(R.id.cardView1);
+		cardView2 = (CardView) findViewById(R.id.cardView2);
+		txCash = (TextView) findViewById(R.id.tx_cash);
+	}
+
+	/**
+	 * set cash
+	 * @param cash buy-in chips
+	 */
+	public void setCash(long cash) {
+		if (cash < 0) {
+			txCash.setText("");
+		} else {
+			txCash.setText(Util.formatNumber(cash));
+		}
+	}
+
+	/**
+	 * set the image of player
+	 * @param profileImage player image
+	 */
+	public void setProfileImage(final Bitmap profileImage) {
+		this.profileImage.setImageBitmap(profileImage);
+	}
+
+	/**
+	 * set the CardView of PlayerView
+	 * @param hand two cards
+	 */
+	public void setHand(Hand hand) {
+		cardView1.setCard(hand.getCard1());
+		cardView2.setCard(hand.getCard2());
+	}
+
+	public void setState(Player.PlayerState playerState) {
+		// TODO implement better background colors
+		switch (playerState) {
+			case FOLD:
+				profileImage.setBackgroundColor(Color.RED);
+				break;
+			case RAISE:
+			case ALL_IN:
+				profileImage.setBackgroundColor(Color.YELLOW);
+				break;
+			case CALL:
+			case CHECK:
+				profileImage.setBackgroundColor(Color.GREEN);
+				break;
+			case ACTIVE:
+				profileImage.setBackgroundColor(Color.TRANSPARENT);
+				break;
+			default:
+				profileImage.setBackgroundColor(Color.LTGRAY);
+				break;
+		}
+
+	}
+
 }
