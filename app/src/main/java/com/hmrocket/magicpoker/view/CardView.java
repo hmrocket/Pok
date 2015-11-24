3a4daff52d840b58f76d56e882b2f06c3eae1615
hmrocket <hmrocket@gmail.com>
2015-11-24 15:44:24 -0500

    add view manipulation methods (reset, facedown) and make the CardView hold jsut the d of the Card not the Card object itself

--- app/src/main/java/com/hmrocket/magicpoker/view/CardView.java
+++ app/src/main/java/com/hmrocket/magicpoker/view/CardView.java
@@ -1,9 +1,12 @@
 package com.hmrocket.magicpoker.view;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
+import com.hmrocket.magicpoker.R;
 import com.hmrocket.magicpoker.Util;
 import com.hmrocket.poker.card.Card;
 
@@ -12,30 +15,71 @@ import com.hmrocket.poker.card.Card;
  */
 public class CardView extends ImageView {
 
-	private Card card;
+	private int cardId;
+	private boolean faceDown;
 
 	public CardView(Context context) {
 		super(context);
+		init();
+	}
+
+	protected void init() {
+		cardId = -1;
+		// by default show the card facedown
+		facedown(true);
+	}
+
+	/**
+	 * Define whether the back or the front of CardView is shown
+	 *
+	 * @param facedown true show card face down, show card face up otherwise
+	 */
+	public void facedown(boolean facedown) {
+		// do not update CardView if the states match
+		if (facedown == this.faceDown && cardId != -1)
+			return;
+		else
+			this.faceDown = facedown;
+
+		if (!facedown) {
+			if (this.cardId != -1) {
+				// setImageResource ==> reading and decoding on the UI thread
+				// setImageDrawable ==> just the setting is on UI thread
+				setImageResource(cardId);
+			} else {
+				setImageBitmap(null);
+			}
+		} else {
+			// just show the back cover of the card
+			setImageResource(R.mipmap.backcover);
+		}
 	}
 
 	public CardView(Context context, AttributeSet attributeset) {
 		super(context, attributeset);
+		init();
 	}
 
-	public Card getCard() {
-		return card;
+	public CardView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		init();
+	}
+
+	@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+	public CardView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+		super(context, attrs, defStyleAttr, defStyleRes);
+		init();
 	}
 
 	public void setCard(Card card) {
-		this.card = card;
-		if (this.card != null) {
-			// setImageResource ==> reading and decoding on the UI thread
-			// setImageDrawable ==> just the setting is on UI thread
-			setImageResource(Util.getCardImageId(card));
-		} else {
-			setImageBitmap(null);
-		}
+		cardId = Util.getCardImageId(card);
 	}
 
+	/**
+	 * Reset the CardView
+	 */
+	public void reset() {
+		init();
+	}
 }
 
