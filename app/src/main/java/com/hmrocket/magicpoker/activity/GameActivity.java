a9589515a3fff8e2a5729db805c75befc9ef5275
Mhamed <hlioui@thoughttechnology.com>
2015-11-30 11:14:29 -0500

    remove fullscreen support from GameActivity

--- app/src/main/java/com/hmrocket/magicpoker/activity/GameActivity.java
+++ app/src/main/java/com/hmrocket/magicpoker/activity/GameActivity.java
@@ -55,16 +55,6 @@ public class GameActivity extends Activity implements View.OnClickListener, Rais
 		setContentView(R.layout.activity_game);
 
 
-		// Hide Navigation and status bar
-		// FIXME keep Navigation and status bar for android-18 and below
-		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-				| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-				| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-				| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-				| View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-				| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-
-
 		// set Layout controller
 		btnController = new Button[5];
 		btnController[0] = (Button) findViewById(R.id.btn_allin);
